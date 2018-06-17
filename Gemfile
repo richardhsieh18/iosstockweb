4cd65acdc464fc71e27cefd646997f0a3b4d3cce
Richard <starengram@gmail.com>
2018-06-17 16:52:07 +0800

    render session_id,email done

--- Gemfile
+++ Gemfile
@@ -36,9 +36,6 @@ gem 'jbuilder', '~> 2.5'
 gem 'devise'
 gem 'rack-cors', require: 'rack/cors'
 
-
-
-
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
---
+++ app/controllers/v1/sessions_controller.rb
@@ -0,0 +1,16 @@
+class V1::SessionsController < ApplicationController
+	protect_from_forgery with: :null_session #CSRF Exception
+	def create
+		user = User.where(email: params[:email]).first
+
+		if user.valid_password?(params[:password])
+			render json: user.as_json(only: [:id, :email]), status: :created
+		else
+			head(:unauthroized)
+		end
+	end
+
+	def destroy
+		
+	end
+end
