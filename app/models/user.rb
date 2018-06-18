1ef0590ab8151cf8ba0e10a085edd76fa6c5dd76
Richard <starengram@gmail.com>
2018-06-19 00:32:40 +0800

    用mail寄認證信搞定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-         #:confirmable, :token_authenticatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable#, :token_authenticatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  #devise_for :users
+  devise_for :users
   namespace :v1 do
   	resources :episodes
   	resources :sessions, only: [:create, :destroy]
